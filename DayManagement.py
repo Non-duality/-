1ace67ca05383387ae0fbde7ce0fc03301d5fa17
Non-duality <non_duality@naver.com>
2020-06-04 00:31:16 +0900

    [Add] - [달력 UI 수정, 데이터 타입 변환(미완성)]

--- DayManagement.py
+++ DayManagement.py
@@ -1,13 +1,15 @@
 import sys
 from datetime import datetime, time
 from functools import partial
-from PyQt5.QtCore import Qt, QThread, pyqtSignal, QRect
+from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
-from main import MainWindow as mw
+from PyQt5.QtGui import *
+from PyQt5 import uic
 import time as stime
 import sort_util
 
 sort_task_list = []
+sub_ui = uic.loadUiType('_uiFiles/sub.ui')[0]
 
 
 class TimeThread(QThread):
@@ -29,8 +31,14 @@ class TimeThread(QThread):
 
     def latest_task_time(self):
         global sort_task_list
-        # task_list의 첫 번쨰 키를 : 스플릿 함
-        latest_time_keys = list(sort_task_list[0].keys())[0].split(':')
+        # sort_task_list의 첫 번쨰 키를 : 스플릿 함
+        latest_time_keys = list(list(sort_task_list[0].values())[0].keys())[0].split(':')
+
+        date_y_m_d = list(sort_task_list[0].keys())[0].split(':')
+        self.latest_time_year = int(date_y_m_d[0])
+        self.latest_time_month = int(date_y_m_d[1])
+        self.latest_time_day = int(date_y_m_d[2])
+
         # self.am_pm_toggle_value가 PM 이면 12를 더해줘여함 datetime에서 1시는 13시로 표현함
         if latest_time_keys[0] == 'PM':
             if not latest_time_keys[1] == '12':
@@ -43,7 +51,7 @@ class TimeThread(QThread):
             self.latest_time_hour = int(latest_time_keys[1])
             self.latest_time_minute = int(latest_time_keys[2])
 
-        return self.latest_time_hour, self.latest_time_minute
+        return self.latest_time_hour, self.latest_time_minute, self.latest_time_year, self.latest_time_month, self.latest_time_day
 
     # TimeThread를 .start() 를하면 실행되는 함수
     def run(self):
@@ -54,23 +62,32 @@ class TimeThread(QThread):
             if sort_task_list == []:
                 break
             # 제일 처음 task의 시간, 분 을 받아옴
-            hour, minute = self.latest_task_time()
+            hour, minute, year, month, day = self.latest_task_time()
             # 현재 시간 분을 받아옴
             now_hour = datetime.today().time().hour
             now_minute = datetime.today().time().minute
-            stime.sleep(30)
+            now_year = datetime.today().year
+            now_month = datetime.today().month
+            now_day = datetime.today().day
+
+            stime.sleep(5)
             # 현재시각을 task_list 제일 처음의 시각을 받아서 비교해서 시간과 분이 같으면 메세지 실행
             if now_hour == hour and now_minute == minute:
-                # print(datetime.today().time())
-                # print(time(hour,minute))
                 # 메인 view에서 messagebox와 connect와 연결된 함수를 싱햄
                 self.messagebox.emit()
                 # os.system('open /Users/goseonghyeon/study/ScheduleManagement/black.mp3')
                 stime.sleep(0.5)
                 sort_task_list.pop(0)
 
+            if year < now_year :
+                sort_task_list.pop(0)
+            elif month < now_month :
+                sort_task_list.pop(0)
+            elif day < now_day :
+                sort_task_list.pop(0)
 
-class DayManagement(QWidget):
+
+class DayManagement(QWidget, sub_ui):
     am_pm_toggle_value = 'PM'
     task_list = []
     thread_count = 0
@@ -81,82 +98,15 @@ class DayManagement(QWidget):
         self.initUI()
 
     def initUI(self):
-        # 레이아웃은 그리드 레이아웃으로 설정
-        grid = QGridLayout()
+        self.setupUi(self)
+        self.setWindowTitle(' Task')
+        self.setWindowIcon(QIcon('image/icon.png'))
 
-        # AM_PM_toggle
-        # AM 과 PM을 설정하기 위한 토글 라벨을 설정한다
-        am_or_pm_toggle_label = QLabel("AM PM Toggle")
-        am_or_pm_toggle_label.setAlignment(Qt.AlignCenter)
-        # AM 과 PM을 설정하기 위해서 버튼을 만든다
-        self.am_or_pm_toggle_button = QPushButton("PM")
-
-        # am_or_pm_toggle_label을 grid 레이아웃에 1행에 0열에 설정
-        grid.addWidget(am_or_pm_toggle_label, 1, 0)
-
-        # am_or_pm_toggle_button을 grid 레이아웃에 2행에 0열에 설정
-        grid.addWidget(self.am_or_pm_toggle_button, 2, 0)
-        # am_or_pm_toggle_button을 토글 버튼으로 만든다
-        self.am_or_pm_toggle_button.setCheckable(True)
-
-        # TIME 설정
-        # 시간을 설정하기 위한 시간 라벨을 설정한다
-        set_time_label = QLabel('TIME')
-        # set_time_label을 중간으로 설정한다
-        set_time_label.setAlignment(Qt.AlignCenter)
-        # set_time_label을 grid 레이아웃에 3행 0열에 설정
-        grid.addWidget(set_time_label, 3, 0)
-
-        # 수평에 대한 레이아웃 생성
-        self.set_time_layout = QHBoxLayout()
-        # 중앙을 맞추기 위한 왼쪽 공백 생성
-        self.set_time_layout.addStretch()
-        # 시간, 분 을 받기위한 텍스트
-        self.set_time_hours = QLineEdit()
-        self.set_time_minutes = QLineEdit()
-        # 시간, 분 텍스트 사이즈 조정
-        self.set_time_hours.setMaximumSize(25, 20)
-        self.set_time_minutes.setMaximumSize(25, 20)
-        # 시간, 분 수평 레이아웃(set_time_layout) 집어넣어 설정함
-        self.set_time_layout.addWidget(self.set_time_hours)
-        self.set_time_layout.addWidget(QLabel(':'))
-        self.set_time_layout.addWidget(self.set_time_minutes)
-        # 중앙을 맞추기 위한 오른쪽 공백 생성
-        self.set_time_layout.addStretch()
-
-        # set_time_layout를 grid레이아웃에 추가함
-        grid.addLayout(self.set_time_layout, 4, 0)
-
-        # task 라벨 추가
-        self.set_task_text_label = QLabel("TASK")
-        # set_task_text_label 중앙 정렬
-        self.set_task_text_label.setAlignment(Qt.AlignCenter)
-        # grid 레이아웃에 5행에 정렬
-        grid.addWidget(self.set_task_text_label, 5, 0)
-        # task 내용 text 설정
-        self.set_task_text = QTextEdit()
-        # grid 레이아웃에 task text 6행에 정렬
-        grid.addWidget(self.set_task_text, 6, 0)
-
-        # 시간과 할일을 저장하는 save 버튼
-        self.task_save_button = QPushButton("save")
-        # task_save_button을 def task_save 함수랑 연결
         self.task_save_button.clicked.connect(self.task_save)
-        # self.task_save_button을 gird 레이아웃에 7행에 정렬
-        grid.addWidget(self.task_save_button, 7, 0)
-
-        # 지금까지 저장한 task 목록을 보여주는 버튼
-        self.see_task_list_button = QPushButton("task list")
-        # see_task_list 함수랑 see_task_list_button 버튼을 연결
         self.see_task_list_button.clicked.connect(self.see_task_list)
-        # see_task_list_button 8행에 정렬
-        grid.addWidget(self.see_task_list_button, 8, 0)
-        # am_or_pm_toggle_button을 클릭하면 self.am_pm_toggle함수를 실행한다
+        self.am_or_pm_toggle_button.setCheckable(True)
         self.am_or_pm_toggle_button.clicked.connect(self.am_pm_toggle)
-
-        self.setLayout(grid)
-        self.setGeometry(300, 300, 540, 300)
-
+ 
     def see_task_list(self):
         '''
         새로운 윈도우 창을 여는 함수로 TaskList를 불러와서 show 새로운 창을 염
@@ -171,15 +121,23 @@ class DayManagement(QWidget):
         global sort_task_list
         if self.time_check(self.am_pm_toggle_value, self.set_time_hours.text(), self.set_time_minutes.text()):
             # am_pm을 받고 ap_or_pm:시간:분 저장
+            year_month_day = "{year}:{month}:{day}:{week}".format(year = self.text_year.text(),
+                                                                  month = self.text_month.text(),
+                                                                  day = self.text_day.text(),
+                                                                  week = self.text_week.text())
+
             hours_minutes = "{am_or_pm}:{hours}:{minutes}".format(am_or_pm=self.am_pm_toggle_value,
                                                                   hours=self.set_time_hours.text(),
                                                                   minutes=self.set_time_minutes.text())
+
             # am_or_pm:시간:분 : 할일 해서 딕셔너리로 저장
-            time_and_task = {hours_minutes: self.set_task_text.toPlainText()}
+            time_and_task = {hours_minutes : self.set_task_text.toPlainText()}
+            overall_task = {year_month_day : time_and_task}
             
             # 시간과 할일을 task_list에 저장한다
-            sort_task_list.append(time_and_task)
-            sort_task_list = sort_util.task_list_sort(sort_task_list)
+            sort_task_list.append(overall_task)
+            sort_task_list = sort_util.date_sort(sort_task_list)
+
             # 시간과 할일을 저장 후에 text 칸을 clear 시킨다
             self.set_time_hours.clear()
             self.set_time_minutes.clear()
@@ -200,6 +158,7 @@ class DayManagement(QWidget):
                 self.thread_count += 1
         else:
             pass
+        return sort_task_list
 
     def open_message_box(self):
         global sort_task_list
@@ -236,9 +195,9 @@ class DayManagement(QWidget):
             # am을 안한 이유는 밤에 작업할때 11:00에 작업중이면 그 다음날 새벽 까지도 작업할 수 있기 때문에 am은 제외
             if not hour == '12':
                 hour = int(hour) + 12
-            if time(int(hour), int(minute)) < datetime.today().time():
-                QMessageBox.about(self, 'Error', '{}:{}이후의 시간을 입력해주세요.'.format(now_hour, now_minute))
-                return False
+            # if time(int(hour), int(minute)) < datetime.today().time():
+            #     QMessageBox.about(self, 'Error', '{}:{}이후의 시간을 입력해주세요.'.format(now_hour, now_minute))
+            #     return False
             else:
                 return True
         return True
--- main.py
+++ main.py
@@ -5,12 +5,10 @@ from PyQt5.QtCore import *
 from PyQt5 import uic
 from DayManagement import *
 
-
 main_ui = uic.loadUiType('_uiFiles/main.ui')[0]
 
 class MainWindow (QMainWindow, main_ui):
-    global sort_task_list
-
+    
     def __init__(self):
         super().__init__()
         self.initUI()
@@ -22,39 +20,46 @@ class MainWindow (QMainWindow, main_ui):
         self.new_window = DayManagement() 
 
         # 메뉴바
-        exitAction = QAction(QIcon('image/exit.png'),'Exit', self)
+        exitAction = QAction(QIcon('image/exit.png'),'EXIT', self)
         exitAction.setShortcut('Ctrl+Q')
         exitAction.triggered.connect(qApp.quit)
 
         menubar = self.menuBar()
         menubar.setNativeMenuBar(False)
-        filemenu = menubar.addMenu('&File')
+        filemenu = menubar.addMenu('&FILE')
         filemenu.addAction(exitAction)
 
         # 클릭 함수
-        self.calendarWidget.clicked[QDate].connect(self.showDate)
-        self.pushButton.clicked.connect(self.clicked_option) 
-
+        self.calendarWidget.clicked[QDate].connect(self.showData)
         date = self.calendarWidget.selectedDate()
-        self.showDate(date)
+        self.pushButton.clicked.connect(self.clicked_option)
 
+        self.showData(date)
+
+    def sortDate(self,date):
+        #Pyqt5.Qtcore.QDate는 데이터를 '월 5 13 2020' 식으로 데이터를 반납한다.
+        #그리하여 이를 한국식으로 변환함
+        temp = date.toString().split(' ')
+        time = "{}년 {}월 {}일 {}요일".format(temp[3],temp[1],temp[2],temp[0])
+        return time
     
+    def splitDate(self,date):
+        temp = date.toString().split(' ')
+        return temp
+
     def clicked_option(self):
         self.new_window.show()
     
-    def showDate(self, date):
-        temp = date.toString().split(' ')
-        time = "{}년 {}월 {}일 {}요일".format(temp[3],temp[1],temp[2],temp[0])
-        self.time_label.setText(time)
-    
-    def test(self,date):
-        for index, work in enumerate(sort_task_list):
-            # 각 할 일 마다 am_or_pm, hours, minutes 구함
-            am_or_pm, hours, minutes = list(work.keys())[0].split(':')
-            # 할 일을 구함
-            task = list(work.values())[0]
-            string = "{} {} : {}\n {}".format(am_or_pm, hours, minutes, task)
-            self.task_label.setText(string)
+    def showData(self,date):
+        # Label에 데이터를 기록하는 함수
+        temp = self.splitDate(date)
+
+        self.time_label.setText(self.sortDate(date))
+
+        self.new_window.text_year.setText(temp[3])
+        self.new_window.text_month.setText(temp[1])
+        self.new_window.text_day.setText(temp[2])
+        self.new_window.text_week.setText(temp[0])
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
